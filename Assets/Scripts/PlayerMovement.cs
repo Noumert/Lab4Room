a9380a53ff6c8d399253b18af6242527b7932575
Kostya Lazanchuk <90553568+KostyaLazanchuk@users.noreply.github.com>
2021-12-26 19:02:17 +0200

    Add Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,31 +1,42 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
-    public Camera cam;
 
     Vector2 movemnt;
     Vector2 mousePos;
 
+    PhotonView view;
+
+    void Start()
+    {
+        view = GetComponent<PhotonView>();
+    }
     void Update()
     {
-       
-        movemnt.x = Input.GetAxisRaw("Horizontal");
-        movemnt.y = Input.GetAxisRaw("Vertical");
+        if (view.IsMine)
+        {
+            Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            Vector2 moveAmount = moveInput.normalized * moveSpeed * Time.deltaTime;
+            transform.position += (Vector3)moveAmount;
+            mousePos = Camera.current.ScreenToWorldPoint(Input.mousePosition);
+        }
 
-        mousePos = cam.ScreenToWorldPoint(Input.mousePosition); 
+        /* movemnt.x = Input.GetAxisRaw("Horizontal");
+         movemnt.y = Input.GetAxisRaw("Vertical");
+
+         mousePos = Camera.current.ScreenToWorldPoint(Input.mousePosition);*/
     }
-
-    // Update is called once per frame
-    void FixUpdate()
+    void FixedUpdate()
     {
         rb.MovePosition(rb.position + movemnt * moveSpeed * Time.fixedDeltaTime);
         Vector2 lookDir = mousePos - rb.position;
-        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg-90f;
-        rb.rotation = angle; 
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+        rb.rotation = angle;
     }
 }
--- Assets/Scripts/SpawnPlayers.cs
+++ Assets/Scripts/SpawnPlayers.cs
@@ -7,6 +7,7 @@ public class SpawnPlayers : MonoBehaviour
 {
     public GameObject player;
     public float minX, minY, maxX, maxY;
+    
     void Start()
     {
         Vector2 randomPosition = new Vector2(Random.Range(52,60),Random.Range(-70,-59));
